2c284866e4bd2465b4021f32c89911c8131bdcf7
edonovanmv-droid <edonovan.mv@gmail.com>
2026-09-19 17:26:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let dinero = 100000;
-
+ 
 function perforar() {
-alert("¡Perforación realizada!");
-}
 
-console.log("Explorador de Vetas Pro iniciado");
+let ley = (Math.random() * 20).toFixed(2);
+
+let exito = Math.random();
+ 
+if (exito > 0.65) {
+
+document.getElementById("resultado").innerHTML =
+"✅ Veta encontrada<br>Ley: "
++ ley +
+" g/t Au";
+ 
+dinero += Math.round(ley * 1000);
+ 
+} else {
+ 
+document.getElementById("resultado").innerHTML =
+"❌ Sin mineralización";
+
+dinero -= 5000;
+}
+ 
+document.getElementById("dinero").innerText =
+dinero;
+}
